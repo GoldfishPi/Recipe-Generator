12acb2e1455508be72daa0c55d275ff919e75bd7
GoldfishPi <erikbadger777@gmail.com>
2018-03-04 15:35:17 -0600

    Created Basic Ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { HeaderComponent } from './header/header/header.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
